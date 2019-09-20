c0fae9b6a49e4b0c366af2383cc0f89ad2b041f9
mtsobgni <benito.tsobgni@yahoo.fr>
2019-09-20 10:55:41 +0200

    Last commit

--- src/main/java/fr/meritis/sg/App.java
+++ src/main/java/fr/meritis/sg/App.java
@@ -1,14 +1,33 @@
 package fr.meritis.sg;
 
+import java.util.*;
+
 public class App {
 
     public static void main (String [] args){
-        //int[] tab = {1,2,2,4};
-        int[] tab = new int[100000000];
-        for (int i=0; i<10000000; i++){
-            tab[i]= (int)Math.random();
-        }
-        //int[] tab = null;
+       int[] tab = {34,34,67,4,6,8,-1,6,12,56,34};
+
         System.out.println(Service.max(tab));
+        System.out.println(Service.maxSort(tab));
+        System.out.println(Service.maxStream(tab));
+
+        HashMap<String, Integer> result= Service.max2(tab);
+        result.forEach((key, value) -> {
+            System.out.println(key+ ":" +value);
+        });
+
+
+        Deque<Double> deque =  new ArrayDeque<>();
+
+        deque.add(11.0);
+        deque.add(21.0);
+        deque.add(15.0);
+        List<Integer> test =  Arrays.asList(1,11,2,3,4,9);
+        List<String> test1 =  Arrays.asList("er", "hdht", "jdh","tet");
+
+        System.out.println(Service.max(deque));
+        System.out.println(Service.max(test));
+        System.out.println(Service.max(test1));
     }
+
 }
--- src/main/java/fr/meritis/sg/Service.java
+++ src/main/java/fr/meritis/sg/Service.java
@@ -9,8 +9,10 @@ public class Service<T> {
 
     public static final String MAX = "Max of elements";
     public static final String MAX2 = "Second Max of elements";
+    public static final int CONST= 2;
 
-
+    /* Question 1  a basic algorith to determine a max of array
+     * */
     public static int max(int [] tab){
 
         if (tab==null){
@@ -30,13 +32,33 @@ public class Service<T> {
 
     }
 
+    /* Question 2  Alternative solution to find a greatest Number in Array
+     * */
+    public static int maxStream(int[] tab){
+
+        return  Arrays.stream(tab)
+                .max()
+                .getAsInt();
+    }
+
+    /* Question 2  Alternative solution to find a greatest Number in Array
+     * */
+
+    public static int maxSort(int [] tab){
+        if(tab!= null && tab.length>0){
+            Arrays.sort(tab);
+            return tab[tab.length-1];
+        }
+        return -1;
+    }
+    /* Question 3  algorith to determine the Top 2 Greatest int values from the input array.
+     * */
     public static HashMap<String, Integer> max2(int [] tab){
 
-        final String MAX = "Max";
         if (tab==null){
             return null;
         }
-        else{
+        else if (tab.length>=CONST){
             int max;
             int secondGreatest;
             if( tab[0]< tab[1]){
@@ -63,19 +85,27 @@ public class Service<T> {
             result.put(MAX2, secondGreatest);
             return result;
         }
+        else{
+            return null;
+        }
 
     }
 
+    /* Question 4  a generic function to determine the highest element of Collection
+     * */
     public static <T extends Object & Comparable<? super T>> T max(Collection<? extends T> coll) {
-        Iterator<? extends T> i = coll.iterator();
-        T candidate = i.next();
+        if (coll != null) {
+            Iterator<? extends T> i = coll.iterator();
+            T candidate = i.next();
 
-        while (i.hasNext()) {
-            T next = i.next();
-            if (next.compareTo(candidate) > 0)
-                candidate = next;
+            while (i.hasNext()) {
+                T next = i.next();
+                if (next.compareTo(candidate) > 0)
+                    candidate = next;
+            }
+            return candidate;
         }
-        return candidate;
+        else return null;
     }
 
 
